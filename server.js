3a6bdeffafb322f0cb9bde392c01709f5fbbb740
waelbenn <103936083+waelbenn@users.noreply.github.com>
2022-07-29 10:54:59 +0100

    deploy

--- server.js
+++ server.js
@@ -42,12 +42,11 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 ); */
 
-if(process.env.NODE_ENV === 'production'){
+
   app.use(express.static('frontend/build'))
   app.get('*', (req,res)=>{
       res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   })
-}
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
